3376c0d2a0441a7c9474032e64772dae70456e3d
vinod14293 <vinod14293@gmail.com>
2019-12-19 12:20:04 +0530

    Authetication part done with error handling

--- src/app/Auth/auth.service.ts
+++ src/app/Auth/auth.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Login } from '../Models/login';
-import {  }
+import { HttpClient } from '@angular/common/http';
+import { AuthResponseData } from '../Model/AuthResponseData';
 
 
 @Injectable({providedIn : 'root'})
 export class Authentication{
 
-constructor(){
+constructor(private http : HttpClient){
 }
 
 authenticate(login : Login){
 console.log('Entered inside authentication method');
-  if(login.email == "vinod@gmail.com" && login.password == "vinod123"){
-    console.log('user authenticated');
-  }
+  // if(login.email == "vinod@gmail.com" && login.password == "vinod123"){
+ //   console.log('user authenticated');
+//  }
+  return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDdILz7MvoRpo_LfNQVq1SvWbjMYBGEo38',{
+  email : login.email,
+  password : login.password,
+  returnSecureToken : true
+});
 
 
 }
--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Authentication  } from '../Auth/auth.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,11 @@ import { Authentication  } from '../Auth/auth.service';
 })
 export class LoginComponent  {
 
-  constructor(private fb : FormBuilder, private login : Authentication){
+  error : any;
+
+  isLoading = false;
+
+  constructor(private fb : FormBuilder, private login : Authentication, private router : Router){
   }
 
 
@@ -20,7 +25,27 @@ export class LoginComponent  {
   });
 
   onSubmit(){
+    this.isLoading = true;
     console.log(this.loginForm.value);
-    this.login.authenticate(this.loginForm.value);
+    this.login.authenticate(this.loginForm.value).subscribe(
+      data => {
+        console.log(data);
+        this.isLoading = false;
+        this.router.navigate(['/dashBoard']);
+      },
+      error => {
+        this.error = error.error.error.message;
+        if(error.error.error.message == 'EMAIL_NOT_FOUND'){
+            this.error="Email doesn't exists";
+        }
+        if(error.error.error.message == 'INVALID_PASSWORD'){
+            this.error="Password is wrong";
+        }
+        
+        this.isLoading = false;
+        console.log('error is '+error.error.error.message);
+      }
+    )
+    ;
   }
 }
