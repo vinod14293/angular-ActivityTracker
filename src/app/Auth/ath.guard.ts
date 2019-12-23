711c378dd7693a9bbab1905849674d9d32d1166e
vinod14293 <vinod14293@gmail.com>
2019-12-23 12:28:23 +0530

    Authentication implemented

--- src/app/Auth/ath.guard.ts
+++ src/app/Auth/ath.guard.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from 'angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Authentication } from './auth.service';
+import { User } from '../Models/User';
+import { take } from 'rxjs/operators'; 
 
 @Injectable({providedIn:"root"})
 export class AuthGuard implements CanActivate{
 
 constructor(private router:Router, private auth : Authentication){
 }
-//user : string;
+user : User;
 
 canActivate(route : ActivatedRouteSnapshot, state : RouterStateSnapshot){
-/*this.auth.User1.subscribe(data =>
+this.auth.User1.pipe(take(1)).subscribe(data =>
 {
-  console.log("email extracted from subscribed data "+data.email);
-this.user=data.email;
-console.log("this.user is "+this.user);
+console.log("email extracted from subscribed data "+data.email);
+this.user=data;
+console.log("this.user is "+JSON.stringify(this.user));
 }
 )
 if (this.user != null || this.user != ""){
@@ -23,9 +25,9 @@ if (this.user != null || this.user != ""){
 }
 else {
   this.router.navigate(['/login']);
-}*/
+  return false;
+}
 
-return true;
 
 }
 
